dd2d10aae859401e24b6b06f2c07f67805aad3d6
pome-ta <pome-ta@users.noreply.github.com>
2021-10-20 15:40:40 +0900

    up

--- logic.py
+++ logic.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 
-
 kifu_path = Path('./kifu.csa')
 
 with kifu_path.open(encoding='utf-8') as f:
   kifu_data = f.readlines()
-
 
 
 class GameLogic:
@@ -15,20 +13,25 @@ class GameLogic:
     self.prompt = [i.strip() for i in kifu[17:-1]]
     self.length_turn = len(self.prompt)
     self.game_board = self.init_board()
-    
+
+    self.sen_hand = []
+    self.kou_hand = []
+
   def init_board(self):
     setlist = []
     for set in self.board_sets:
       # todo: 3つのchar で処理させ、分割させる
       x_line = '_' + set.strip()
-      line = [x_line[i:i +3].strip() for i in range(0, len(x_line), 3)]
+      line = [x_line[i:i + 3].strip() for i in range(0, len(x_line), 3)]
       setlist.append(line[1:])
     return setlist
-    
+
   def turn_purser(self, turn_data):
-    self.piece_name = 'None'
-    self.af_x = 0
-    self.af_y = 0
+    turn_num = self.prompt.index(turn_data)
+    piece_name = 'None'
+    af_x = 0
+    af_y = 0
+
     if len(turn_data) == 1:
       self.game_board = self.init_board()
       print('開始')
@@ -39,7 +42,8 @@ class GameLogic:
     turn = turn_data[0]
     _before = turn_data[1:3]
     _after = turn_data[3:5]
-    self.piece_name = turn + turn_data[5:]
+    piece_name = turn + turn_data[5:]
+    print(f'{turn_num: 3d}手目:', _after + piece_name)
     if not '00' in _before:
       be_y = 9 - int(_before[0])
       be_x = int(_before[1]) - 1
@@ -47,12 +51,12 @@ class GameLogic:
     else:
       be_y = None
       be_x = None
-    self.af_y = 9 - int(_after[0])
-    self.af_x = int(_after[1]) - 1
-    self.game_board[self.af_x][self.af_y] = self.piece_name
-    
+    af_y = 9 - int(_after[0])
+    af_x = int(_after[1]) - 1
+    self.game_board[af_x][af_y] = piece_name
+
   def print_board(self):
-    print(f'{self.af_x}, {self.af_y}, {self.piece_name}')
+    print(f'後攻手駒: {self.kou_hand}')
     print('+---------------------------+')
     for board in self.game_board:
       line = ' '
@@ -62,12 +66,13 @@ class GameLogic:
         line += piece
       print(line)
     print('+---------------------------+')
-  
-  
+    print(f'先攻手駒: {self.sen_hand}')
+
 
 if __name__ == '__main__':
   game = GameLogic(kifu_data)
-  for p in game.prompt:
+  for n, p in enumerate(game.prompt):
     game.turn_purser(p)
     game.print_board()
     input()
+
