634482a4a3fe6bcf742cbd908058c6c3c9fa6faf
pome-ta <pometarou93@gmail.com>
2021-10-22 21:55:32 +0900

    set up reader

--- reader.py
+++ reader.py
@@ -6,17 +6,31 @@ from pathlib import Path
 def load_kifu():
     _path = Path('./kifu.csa')
     with _path.open() as f:
-        kifu_data = f.readlines()
-    return kifu_data
+        _kifu_data = f.readlines()
+    return _kifu_data
 
 
 class KifuReader:
-    def __init__(self, kifu_data):
-        self.header = kifu_data[:8]
-        self.board_init = kifu_data[8:17]
-        self.prompt = [i.strip() for i in kifu_data[17:-1]]
+    def __init__(self, data):
+        _header = data[:8]
+        board_init = data[8:17]
+        self.prompt = [i.strip() for i in data[17:-1]]
+        self.game_board = self.init_board(board_init)
+
+    @staticmethod
+    def init_board(board):
+        setup_board = []
+        for setup in board:
+            # 3つのchar として分離さす
+            x_line = '_' + setup.strip()
+            one_line = [x_line[i: i + 3].strip() for i in range(0, len(x_line), 3)]
+            setup_board.append(one_line[1:])
+        return setup_board
 
 
 if __name__ == '__main__':
-    kifu = load_kifu()
-    KifuReader(kifu)
+    from pprint import pprint
+
+    kifu_data = load_kifu()
+    kifu = KifuReader(kifu_data)
+    pprint(kifu.prompt)
